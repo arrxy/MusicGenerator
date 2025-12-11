2068a416e24714e53f5fbfe50085548b071ed99b
Aritro <aritroroy1999@gmail.com>
2025-12-10 22:17:06 -0500

    c

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ except ImportError:
 # %%
 # --- FILES ---
 CHECKPOINT_PATH = "ckpt_XL_extended.pt"  # <--- Your specific checkpoint
-TEST_DATA_PATH = "data/processed/test.txt"  # <--- Test data
+TEST_DATA_PATH = "data/processed/val.txt"  # <--- Test data
 VOCAB_PATH = "data/processed/vocab.json"
 
 # --- HARDWARE (H200 Optimized) ---
